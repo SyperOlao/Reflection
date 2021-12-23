d06a103140cadbcec08f9caefef051e2e4086ef6
SyperOlao <olay1234538@gmail.com>
2021-12-24 02:51:52 +0300

    измененеия

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
@@ -31,7 +30,7 @@ namespace WindowsFormsApp1
             var asm = Assembly.Load("ClassLibrary2");
             var types = asm.GetTypes();
             var result = types.Where(x => x.GetInterface("IFurniture") != null);
-            label1.Text = "1";
+            label1.Text = "Выберете класс из списка: ";
             foreach (var r in result)
             {
                 comboBox1.Items.Add(r);
@@ -43,7 +42,6 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             
-        
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -51,27 +49,13 @@ namespace WindowsFormsApp1
             var item = comboBox1.SelectedItem;
             label1.Text = item.ToString();
             var type = typeof(IFurniture);
+            
+            comboBox2.Items.Clear();
             foreach (var method in type.GetMethods())
             {
                 comboBox2.Items.Add(method.Name);
             }
         }
         
-        public static void ShowMethods(Type type)
-        {
-            foreach (var method in type.GetMethods())
-            {
-                var parameters = method.GetParameters();
-                var parameterDescriptions = string.Join
-                (", ", method.GetParameters()
-                    .Select(x => x.ParameterType + " " + x.Name)
-                    .ToArray());
-
-                Console.WriteLine("{0} {1} ({2})",
-                    method.ReturnType,
-                    method.Name,
-                    parameterDescriptions);
-            }
-        }
     }
 }
